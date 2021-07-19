6e8ee0f837021d8e5d96ee96ccd7196d5b427d33
刘亚林 <779292982@qq.com>
2021-07-19 16:19:01 +0800

    修改数据存储接口

--- ifw/src/main/java/com/lyl/ifw/cacheframeapi/IDataStorage.java
+++ ifw/src/main/java/com/lyl/ifw/cacheframeapi/IDataStorage.java
@@ -15,4 +15,8 @@ public interface IDataStorage {
     boolean put(Context context, String key, String subKey, Object object);
 
     boolean put(Context context, String key, String subKey, Collection object);
+
+    Object get(Context context, String key, String subKey);
+
+    <T> T get(Context context, String key, String subKey, Class<T> clazz);
 }
