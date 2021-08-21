65eb1c444f73f80bcdc8ff57203f0440eb941e86
刘亚林 <779292982@qq.com>
2021-08-21 11:31:54 +0800

    添加极光推送模块

---
+++ jpush/src/main/java/com/lyl/jpush/JPushReceiver.java
@@ -0,0 +1,13 @@
+package com.lyl.jpush;
+
+import cn.jpush.android.service.JPushMessageReceiver;
+
+/**
+ * * @Description
+ * * @Author 刘亚林
+ * * @CreateDate 2021/8/21
+ * * @Version 1.0
+ * * @Remark TODO
+ **/
+public class JPushReceiver extends JPushMessageReceiver {
+}
---
+++ jpush/src/main/java/com/lyl/jpush/JPushService.java
@@ -0,0 +1,13 @@
+package com.lyl.jpush;
+
+import cn.jpush.android.service.JCommonService;
+
+/**
+ * * @Description
+ * * @Author 刘亚林
+ * * @CreateDate 2021/8/21
+ * * @Version 1.0
+ * * @Remark TODO jpush服务
+ **/
+public class JPushService extends JCommonService {
+}
